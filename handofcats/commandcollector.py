aa9c97a6b1e22a842506cf0d3936927205bbed7b
podhmo <podhmo+altair@beproud.jp>
2015-11-09 00:21:57 +0900

    scanner split from collector

--- handofcats/commandcollector.py
+++ handofcats/commandcollector.py
@@ -1,22 +1,13 @@
 # -*- coding:utf-8 -*-
 import logging
+import glob
+import os.path
+from importlib import import_module
+
 logger = logging.getLogger(__name__)
 
 
-class CommandCollector(object):
-    def __init__(self):
-        self.managed_set = set()
-
-    def mark(self, target):
-        self.managed_set.add(target)
-        return target
-
-    def is_marked(self, target):
-        return target in self.managed_set
-
-    def collect(self):
-        return self.managed_set
-
+class ModuleScanner(object):
     def get_module_and_pathlist(self, module_or_path, importer):
         if hasattr(module_or_path, "__path__"):
             return module_or_path, module_or_path.__path__
@@ -25,10 +16,6 @@ class CommandCollector(object):
             return module, module.__path__
 
     def scan(self, root, exclude=None):
-        import glob
-        import os.path
-        from importlib import import_module
-
         logging.info("scan: %s", root)
         m, pathlist = self.get_module_and_pathlist(root, importer=import_module)
         root_package = m.__package__ or m.__name__
@@ -44,7 +31,27 @@ class CommandCollector(object):
                 logger.debug("scan target: %s", target)
                 if target.endswith(".py"):
                     target = target[:-3]
-                command_module = target.replace(path, root_package).replace(os.sep, ".")
-                logger.info("scan command: %s", command_module)
-                import_module(command_module)
+                module = target.replace(path, root_package).replace(os.sep, ".")
+                logger.info("scan command: %s", module)
+                yield module
+
+
+class CommandCollector(object):
+    def __init__(self):
+        self.managed_set = set()
+        self.scanner = ModuleScanner()
+
+    def mark(self, target):
+        self.managed_set.add(target)
+        return target
+
+    def is_marked(self, target):
+        return target in self.managed_set
+
+    def collect(self):
+        return self.managed_set
+
+    def scan(self, root, exclude=None):
+        for module in self.scanner.scan(root, exclude=exclude):
+            import_module(module)
         return self.collect()
