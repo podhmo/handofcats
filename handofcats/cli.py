f7113c7e98e3c239319ac0acc4d68c4ac4df6838
podhmo <podhmo+altair@beproud.jp>
2015-11-16 02:42:51 +0900

    support python2.x with filepath:attr format

--- handofcats/cli.py
+++ handofcats/cli.py
@@ -2,10 +2,7 @@
 import argparse
 import sys
 from handofcats import as_command
-try:
-    from importlib import import_module, machinery
-except ImportError:
-    from importlib2 import import_module, machinery
+from importlib import import_module
 
 
 def import_symbol(module, name):
@@ -14,6 +11,32 @@ def import_symbol(module, name):
 
 
 def import_symbol_from_filepath(path, name, module_id=None):
+    try:
+        from importlib import machinery
+    except ImportError:
+        # patching for import machinery
+        # https://bitbucket.org/ericsnowcurrently/importlib2/issues/8/unable-to-import-importlib2machinery
+        import importlib2._fixers as f
+        fix_importlib_original = f.fix_importlib
+
+        def fix_importlib(ns):
+            if ns["__name__"] == 'importlib2.machinery':
+                class _LoaderBasics:
+                    load_module = object()
+                ns["_LoaderBasics"] = _LoaderBasics
+
+                class FileLoader:
+                    load_module = object()
+                ns["FileLoader"] = FileLoader
+
+                class _NamespaceLoader:
+                    load_module = object()
+                    module_repr = object()
+                ns["_NamespaceLoader"] = _NamespaceLoader
+            fix_importlib_original(ns)
+        f.fix_importlib = fix_importlib
+        from importlib2 import machinery
+
     module_id = module_id or path.replace("/", "_").rstrip(".py")
     module = machinery.SourceFileLoader(module_id, path).load_module()
     return getattr(module, name)
