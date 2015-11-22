9389a79d1c45050acd84b86fc18e6d24a21c3ff1
podhmo <podhmo+altair@beproud.jp>
2015-11-23 00:26:05 +0900

    bump version

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ testing_extras = tests_require + [
 ]
 
 setup(name='handofcats',
-      version='0.4.1',
+      version='0.4.2',
       description='python function to command translator',
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
