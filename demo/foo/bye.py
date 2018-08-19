21e643e0334ffefecdc9bf0c9d034258baa431ea
podhmo <ababjam61+github@gmail.com>
2018-08-19 18:02:33 +0900

    docs: fix examples

--- demo/foo/bye.py
+++
@@ -1,7 +0,0 @@
-# -*- coding:utf-8 -*-
-from handofcats import as_command
-
-
-@as_command
-def bye():
-    print("bye")
--- handofcats/parsers/commandline.py
+++ handofcats/parsers/commandline.py
@@ -12,7 +12,6 @@ def create_parser(fn, description=None):
     argspec = inspect.getfullargspec(fn)
     parser = argparse.ArgumentParser(description=fn.__doc__)
     parser.print_usage = parser.print_help
-
     for k in argspec.kwonlyargs:
         parser.add_argument(f'{"-" if len(k) <= 1 else "--"}{k.replace("_", "-")}', required=True)
     return parser
