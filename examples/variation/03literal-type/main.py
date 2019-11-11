3876f4528fd4ac5894f1073962c0c3f1b7ed11f1
podhmo <ababjam61+github@gmail.com>
2019-11-11 16:41:16 +0900

    chore: add examples

---
+++ examples/variation/03literal-type/main.py
@@ -0,0 +1,11 @@
+from handofcats import as_command
+import typing as t
+from typing_extensions import Literal
+
+Mode = Literal["a", "w", "r"]
+Value = Literal[0, 1, -1]
+
+
+@as_command
+def run(filename: str, *, mode: t.Optional[Mode], value: Value) -> None:
+    pass
