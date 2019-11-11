91f40936b1612743b79aa3b7ab92dd246e80812b
podhmo <ababjam61+github@gmail.com>
2019-11-11 18:07:36 +0900

    chore: add examples

---
+++ examples/readme/04literals/dump.py
@@ -0,0 +1,33 @@
+import sys
+import typing as t
+import typing_extensions as tx
+
+def csv_dump(rows: t.Sequence[dict]) -> None:
+    import csv
+    w = csv.DictWriter(sys.stdout, ["name", "age"])
+    w.writeheader()
+    w.writerows(rows)
+
+
+def json_dump(rows: t.Sequence[dict]) -> None:
+    import json
+    json.dump(rows, sys.stdout, indent=2, ensure_ascii=False)
+    sys.stdout.write("\n")
+
+
+DumpFormat = tx.Literal["json", "csv"]
+
+
+def run(*, format: DumpFormat = "json"):
+    rows = [
+        {
+            "name": "foo",
+            "age": 20,
+        },
+        {
+            "name": "bar",
+            "age": 21,
+        },
+    ]
+    dump = globals()["{}_dump".format(format)]
+    dump(rows)
