4fd91fb12787f7e684918153bec4cf866d5b80c5
podhmo <ababjam61+github@gmail.com>
2019-11-11 14:09:24 +0900

    chore: invalid output

--- examples/variation/00positional/_exposed.py
+++ examples/variation/00positional/_exposed.py
@@ -1,13 +1,13 @@
 
 
-def run(filename: str) -> None:
+def run(file_name: str) -> None:
     pass
 
 def main(argv=None):
     import argparse
     parser = argparse.ArgumentParser(description=None)
     parser.print_usage = parser.print_help
-    parser.add_argument('filename')
+    parser.add_argument('file-name')
     args = parser.parse_args(argv)
     run(**vars(args))
 
