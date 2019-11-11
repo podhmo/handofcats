f00666ab7ea989f76b21084502e48adfde173ce3
podhmo <ababjam61+github@gmail.com>
2019-11-11 13:43:36 +0900

    docs: add examples

---
+++ examples/variation/00positional/_exposed.py
@@ -0,0 +1,16 @@
+
+
+def run(filename: str) -> None:
+    pass
+
+def main(argv=None):
+    import argparse
+    parser = argparse.ArgumentParser(description=None)
+    parser.print_usage = parser.print_help
+    parser.add_argument('filename')
+    args = parser.parse_args(argv)
+    run(**vars(args))
+
+
+if __name__ == '__main__':
+    main()
