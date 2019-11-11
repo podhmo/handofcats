79c307c07738d11601bf8e3b55f0a6a0129827d9
podhmo <ababjam61+github@gmail.com>
2019-11-11 14:14:27 +0900

    chore: update examples

--- examples/variation/00positional/_exposed.py
+++ examples/variation/00positional/_exposed.py
@@ -7,7 +7,7 @@ def main(argv=None):
     import argparse
     parser = argparse.ArgumentParser(description=None)
     parser.print_usage = parser.print_help
-    parser.add_argument('file-name')
+    parser.add_argument('file_name')
     args = parser.parse_args(argv)
     run(**vars(args))
 
