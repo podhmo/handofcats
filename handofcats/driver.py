d7aece873a8137b84b4b296bd831435444b016a7
podhmo <ababjam61+github@gmail.com>
2020-01-11 06:15:12 +0900

    refactor: adjustment for di

--- handofcats/driver.py
+++ handofcats/driver.py
@@ -4,14 +4,48 @@ from .actions import TargetFunction
 from . import injectlogging
 
 
+class Executor:
+    injector_class = Injector
+
+    def __init__(self, fn: TargetFunction) -> None:
+        self.fn = fn
+
+    def execute(
+        self,
+        m,
+        parser,
+        argv=None,
+        *,
+        ignore_logging=False,
+        cont: t.Optional[TargetFunction] = None
+    ):
+        cont = cont or self.fn
+
+        injector = self.injector_class(self.fn)
+        injector.inject(parser, callback=m.stmt)
+
+        if not ignore_logging:
+            injectlogging.setup(parser)
+
+        args = parser.parse_args(argv)
+        params = vars(args).copy()
+
+        if not ignore_logging:
+            injectlogging.activate(params)
+
+        return cont(**params)
+
+
 class Driver:
+    executor_class = Executor
+
     def __init__(self, *, ignore_logging=False):
         self.ignore_logging = ignore_logging
 
     def run(
         self, fn: TargetFunction, argv=None,
     ):
-        executor = Executor(fn)
+        executor = self.executor_class(fn)
         if "--expose" in (argv or []):
             return self._run_expose_action(executor, argv)
         else:
@@ -43,35 +77,3 @@ class Driver:
         return executor.execute(
             m, parser, argv, ignore_logging=self.ignore_logging, cont=cont
         )
-
-
-class Executor:
-    injector_class = Injector
-
-    def __init__(self, fn: TargetFunction) -> None:
-        self.fn = fn
-
-    def execute(
-        self,
-        m,
-        parser,
-        argv=None,
-        *,
-        ignore_logging=False,
-        cont: t.Optional[TargetFunction] = None
-    ):
-        cont = cont or self.fn
-
-        injector = self.injector_class(self.fn)
-        injector.inject(parser, callback=m.stmt)
-
-        if not ignore_logging:
-            injectlogging.setup(parser)
-
-        args = parser.parse_args(argv)
-        params = vars(args).copy()
-
-        if not ignore_logging:
-            injectlogging.activate(params)
-
-        return cont(**params)
