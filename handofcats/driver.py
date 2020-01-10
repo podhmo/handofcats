bb358d92020c6049b20ef70d0ab6204cb87c3d0b
podhmo <ababjam61+github@gmail.com>
2020-01-11 07:37:37 +0900

    refactor: remove executor

--- handofcats/driver.py
+++ handofcats/driver.py
@@ -1,28 +1,58 @@
 import typing as t
 import sys
 from .injector import Injector
-from .actions import TargetFunction
+from .actions import TargetFunction, ContFunction
 from . import injectlogging
 
 
-class Executor:
+class Driver:
     injector_class = Injector
 
-    def __init__(self, fn: TargetFunction) -> None:
-        self.fn = fn
+    def __init__(self, *, ignore_logging=False):
+        self.ignore_logging = ignore_logging
 
-    def execute(
+    def run(
+        self, fn: TargetFunction, argv=None,
+    ):
+        if "--expose" in (argv or []):
+            return self._run_expose_action(fn, argv)
+        else:
+            return self._run_command_line(fn, argv)
+
+    def _run_command_line(
+        self, fn: TargetFunction, argv: t.Optional[str] = None
+    ) -> t.Any:
+        from .actions import commandline
+
+        m, parser, cont = commandline.setup(fn)
+        return self._run(
+            m, parser, fn, argv, ignore_logging=self.ignore_logging, cont=cont
+        )
+
+    def _run_expose_action(
+        self, fn: TargetFunction, argv: t.Optional[str] = None
+    ) -> t.Any:
+        from .actions import codegen
+
+        inplace = "--inplace" in (argv or [])
+        typed = "--typed" in (argv or [])
+
+        m, parser, cont = codegen.setup(fn, inplace=inplace, typed=typed)
+        return self._run(m, parser, fn, argv, ignore_logging=True, cont=cont)
+
+    def _run(
         self,
         m,
         parser,
+        fn: TargetFunction,
         argv=None,
         *,
         ignore_logging=False,
-        cont: t.Optional[TargetFunction] = None,
+        cont: t.Optional[ContFunction] = None,
     ):
-        cont = cont or self.fn
+        cont = cont or fn
 
-        injector = self.injector_class(self.fn)
+        injector = self.injector_class(fn)
         injector.inject(parser, callback=m.stmt)
 
         # TODO: include generated code, emitted by `--expose`
@@ -38,60 +68,56 @@ class Executor:
         return cont(params=params)
 
 
-class Driver:
-    executor_class = Executor
+class MultiDriver:
+    injector_class = Injector
 
     def __init__(self, *, ignore_logging=False):
         self.ignore_logging = ignore_logging
+        self.functions: t.List[TargetFunction] = []
+
+    def register(self, fn: TargetFunction) -> None:
+        self.functions.append(fn)
 
     def run(
-        self, fn: TargetFunction, argv=None,
+        self, argv=None,
     ):
-        executor = self.executor_class(fn)
-        if "--expose" in (argv or []):
-            return self._run_expose_action(executor, argv)
+        if "--expose" in (argv or sys.argv[1:]):
+            return self._run_expose_action(self.functions[0], argv)
         else:
-            return self._run_command_line(executor, argv)
+            return self._run_command_line(self.functions[0], argv)
 
     def _run_command_line(
-        self, executor: "Executor", argv: t.Optional[str] = None
+        self, fn: TargetFunction, argv: t.Optional[str] = None
     ) -> t.Any:
         from .actions import commandline
 
-        fn = executor.fn
-        m, parser, cont = commandline.setup(fn)
-        return executor.execute(
-            m, parser, argv, ignore_logging=self.ignore_logging, cont=cont
+        m, parser, cont = commandline.setup_for_multi_command(fn)
+        return self._run(
+            m, parser, fn, argv, ignore_logging=self.ignore_logging, cont=cont
         )
 
     def _run_expose_action(
-        self, executor: "Executor", argv: t.Optional[str] = None,
+        self, fn: TargetFunction, argv: t.Optional[str] = None
     ) -> t.Any:
         from .actions import codegen
 
-        fn = executor.fn
+        inplace = "--inplace" in (argv or sys.argv[1:])
+        typed = "--typed" in (argv or sys.argv[1:])
 
-        inplace = "--inplace" in (argv or [])
-        typed = "--typed" in (argv or [])
+        m, parser, cont = codegen.setup_for_multi_command(
+            fn, inplace=inplace, typed=typed
+        )
+        return self._run(m, parser, fn, argv, ignore_logging=True, cont=cont)
 
-        m, parser, cont = codegen.setup(fn, inplace=inplace, typed=typed)
-        return executor.execute(m, parser, argv, ignore_logging=True, cont=cont)
-
-
-class ExecutorForMultiCommand:
-    injector_class = Injector
-
-    def __init__(self, functions: t.List[TargetFunction]) -> None:
-        self.functions = functions
-
-    def execute(
+    def _run(
         self,
         m,
         parser,
+        fn: TargetFunction,
         argv=None,
         *,
         ignore_logging=False,
-        cont: t.Optional[TargetFunction],
+        cont: t.Optional[ContFunction],
     ):
         # TODO: include generated code, emitted by `--expose`
         if not ignore_logging:
@@ -122,48 +148,3 @@ class ExecutorForMultiCommand:
             injectlogging.activate(params)
 
         return cont(params=params)
-
-
-class MultiDriver:
-    executor_class = ExecutorForMultiCommand
-
-    def __init__(self, *, ignore_logging=False):
-        self.ignore_logging = ignore_logging
-        self.functions: t.List[TargetFunction] = []
-
-    def register(self, fn: TargetFunction) -> None:
-        self.functions.append(fn)
-
-    def run(
-        self, argv=None,
-    ):
-        executor = self.executor_class(self.functions)
-        if "--expose" in (argv or sys.argv[1:]):
-            return self._run_expose_action(executor, argv)
-        else:
-            return self._run_command_line(executor, argv)
-
-    def _run_command_line(
-        self, executor: "Executor", argv: t.Optional[str] = None
-    ) -> t.Any:
-        from .actions import commandline
-
-        fn = executor.functions[0]
-
-        m, parser, cont = commandline.setup_for_multi_command(fn)
-        return executor.execute(
-            m, parser, argv, ignore_logging=self.ignore_logging, cont=cont
-        )
-
-    def _run_expose_action(
-        self, executor: "Executor", argv: t.Optional[str] = None,
-    ) -> t.Any:
-        from .actions import codegen
-
-        fn = executor.functions[0]
-
-        inplace = "--inplace" in (argv or sys.argv[1:])
-        typed = "--typed" in  (argv or sys.argv[1:])
-
-        m, parser, cont = codegen.setup_for_multi_command(fn, inplace=inplace, typed=typed)
-        return executor.execute(m, parser, argv, ignore_logging=True, cont=cont)
