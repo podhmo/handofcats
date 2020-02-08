2f85b1b38a3585ba06728f59bfba4a28455302f2
podhmo <ababjam61+github@gmail.com>
2020-02-08 21:43:44 +0900

    chore: update examples

--- examples/parse-args/05simple/_exposed.py
+++ examples/parse-args/05simple/_exposed.py
@@ -8,8 +8,7 @@ def run(file_name: str, *, nick_name: Optional[str] = None) -> None:
 def main(argv=None):
     import argparse
 
-    parser = argparse.ArgumentParser(prog=run.__name__, description=run.__doc__)
-    parser.print_usage = parser.print_help  # type: ignore
+    parser = argparse.ArgumentParser()
     parser.add_argument('file_name', help='-')
     parser.add_argument('--nick-name', required=False, help='-')
     args = parser.parse_args(argv)
