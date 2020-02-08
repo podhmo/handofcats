d97962147d3b45fb411b3d16887be7b4cd0b85ec
podhmo <ababjam61+github@gmail.com>
2020-02-08 18:00:06 +0900

    chore: update examples

--- examples/_misc/03mypy-exposed-code/01exposed.py
+++ examples/_misc/03mypy-exposed-code/01exposed.py
@@ -10,23 +10,22 @@ def byebye(*, args: t.List[str]) -> None:
     print(f"byebye, {', '.join(args)}")
 
 
-
 def main(argv: t.Optional[t.List[str]] = None) -> t.Any:
     import argparse
 
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(formatter_class=type('_HelpFormatter', (argparse.ArgumentDefaultsHelpFormatter, argparse.RawTextHelpFormatter), {}))
     subparsers = parser.add_subparsers(title='subcommands', dest='subcommand')
     subparsers.required = True
 
     fn = hello
-    sub_parser = subparsers.add_parser(fn.__name__, help=fn.__doc__)
-    sub_parser.add_argument('--name', required=False, default='world', help="(default: 'world')")
-    sub_parser.add_argument('--nickname', required=False)
+    sub_parser = subparsers.add_parser(fn.__name__, help=fn.__doc__, formatter_class=parser.formatter_class)
+    sub_parser.add_argument('--name', required=False, default='world', help='-')
+    sub_parser.add_argument('--nickname', required=False, help='-')
     sub_parser.set_defaults(subcommand=fn)
 
     fn = byebye  # type: ignore
-    sub_parser = subparsers.add_parser(fn.__name__, help=fn.__doc__)
-    sub_parser.add_argument('--args', required=True, action='append')
+    sub_parser = subparsers.add_parser(fn.__name__, help=fn.__doc__, formatter_class=parser.formatter_class)
+    sub_parser.add_argument('--args', required=True, action='append', help='-')
     sub_parser.set_defaults(subcommand=fn)
 
     args = parser.parse_args(argv)
